498ecfe5613dbebadee2b833be4e5ce2ea87bccc
Nikitooos123 <120833063+Nikitooos123@users.noreply.github.com>
2023-08-08 23:18:36 +0300

    Кнопки

---
+++ game_bot/button.py
@@ -0,0 +1,15 @@
+from aiogram.types import KeyboardButton
+
+play_game = [KeyboardButton(text='Хочу играть!')]
+clue = [KeyboardButton(text='/help')]
+statistics = [KeyboardButton(text='/stat')]
+exit = [KeyboardButton(text='/cancel')]
+attemps = [KeyboardButton(text='/attemps')]
+rating = [KeyboardButton(text='/rating')]
+
+key0 = [play_game, clue + statistics]
+key1 = [play_game, statistics + attemps]
+key2 = [play_game, clue + rating]
+key3 = [exit + clue + statistics]
+key4 = [play_game, exit + clue + statistics]
+key5 = [play_game, clue + statistics]
