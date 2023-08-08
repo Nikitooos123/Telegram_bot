138414b3b8b4f1b1ab8b0a38bdaaaca71e4208fa
Nikitooos123 <120833063+Nikitooos123@users.noreply.github.com>
2023-08-08 23:19:05 +0300

    сокращены строчк кода в основном файле

--- game_bot/game_bot.py
+++ game_bot/game_bot.py
@@ -1,11 +1,10 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import Message, ReplyKeyboardMarkup
 from aiogram.filters import Text, Command
 import random
 from TOKEN_API import API_TOKEN
-import pickle
-import asyncio
-
+import button
+from data import users, save
 
 url = 'https://tenor.com/ru/view/bear-clapping-clap-yay-hooray-gif-5605760'
 # Создаем объекты бота и деспетчера
@@ -14,34 +13,13 @@ dp: Dispatcher = Dispatcher()
 
 ATTEMPS: int = 0
 
-
-play_game = [KeyboardButton(text='Хочу играть!')]
-clue = [KeyboardButton(text='/help')]
-statistics = [KeyboardButton(text='/stat')]
-exit = [KeyboardButton(text='/cancel')]
-attemps = [KeyboardButton(text='/attemps')]
-rating = [KeyboardButton(text='/rating')]
-
-key0 = [play_game, clue + statistics]
-key1 = [play_game, statistics + attemps]
-key2 = [play_game, clue + rating]
-key3 = [exit + clue + statistics]
-key4 = [play_game, exit + clue + statistics]
-key5 = [play_game, clue + statistics]
-
-try:
-    with open('data.pickle', 'rb') as file:
-        users: dict = pickle.load(file)
-except FileNotFoundError:
-    users: dict = {}
-
 def random_number() -> int:
     return random.randint(1, 100)
 
 @dp.message(Command(commands=['start']))
 async def start_game(message:Message):
-    # print(message.json(indent=4, exclude_none=True, ensure_ascii=False))
-    keyboard = ReplyKeyboardMarkup(keyboard=key0, resize_keyboard=True, one_time_keyboard=True)
+    print(message.json(indent=4, exclude_none=True, ensure_ascii=False))
+    keyboard = ReplyKeyboardMarkup(keyboard=button.key0, resize_keyboard=True, one_time_keyboard=True)
     await message.answer('Привт!\nДавай сыграем в игру "Угадай число"?\n\n'
                          'Чтобы получить правила игры и список доступных '
                          'команд - отправь команду /help', reply_markup=keyboard)
@@ -59,7 +37,7 @@ async def start_game(message:Message):
 
 @dp.message(Command(commands=['help']))
 async def help_command(message: Message):
-    keyboard = ReplyKeyboardMarkup(keyboard=key1, resize_keyboard=True, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(keyboard=button.key1, resize_keyboard=True, one_time_keyboard=True)
     await message.answer(f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
                          f'а вам нужно его угадать \nУ вас есть {users[message.from_user.id]["attemps"]} '
                          f'попыток\n\nДоступные команды:\n/help - правила\n'
@@ -77,7 +55,7 @@ async def change_attems(message: Message):
 
 @dp.message(Command(commands=['stat']))
 async def start_game(message: Message):
-    keyboard = ReplyKeyboardMarkup(keyboard=key2, resize_keyboard=True, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(keyboard=button.key2, resize_keyboard=True, one_time_keyboard=True)
     await message.answer(f'Всего игр сыграно: {users[message.from_user.id]["total_games"]}\n'
                          f'Игр выиграно: {users[message.from_user.id]["win"]}\n'
                          f'Количество используемых попыток: {users[message.from_user.id]["attemps"]}', reply_markup=keyboard)
@@ -109,10 +87,9 @@ async def cancel_command(message: Message):
         await message.answer('А мы с вами не играем'
                              'Moжет, сыграем разок?')
 
-@dp.message(Text(text=['Да', 'Давай', 'Сыграем', 'Игра', 'Играть', 'Хочу играть!'],
-                 ignore_case=True))
+@dp.message(Text(text=['Да', 'Давай', 'Сыграем', 'Игра', 'Играть', 'Хочу играть!'], ignore_case=True))
 async def process_game(message: Message):
-    keyboard = ReplyKeyboardMarkup(keyboard=key3, resize_keyboard=True, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(keyboard=button.key3, resize_keyboard=True, one_time_keyboard=True)
     if not users[message.from_user.id]['in_game']:
         await message.answer('Ура\n\nЯ загадал число от 1 до 100, '
                              'попробуй угадать!', reply_markup=keyboard)
@@ -126,7 +103,7 @@ async def process_game(message: Message):
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def process_numbers(message: Message):
     global ATTEMPS
-    keyboard = ReplyKeyboardMarkup(keyboard=key4, resize_keyboard=True, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(keyboard=button.key4, resize_keyboard=True, one_time_keyboard=True)
     if users[message.from_user.id]['in_game']:
         if int(message.text) == users[message.from_user.id]['secret_number']:
             await message.answer_document(document=url)
@@ -136,8 +113,7 @@ async def process_numbers(message: Message):
             users[message.from_user.id]['in_game'] = False
             users[message.from_user.id]['total_games'] += 1
             users[message.from_user.id]['win'] += 1
-            with open('data.pickle', 'wb') as file:
-                pickle.dump(users, file)
+            save()
         elif int(message.text) > users[message.from_user.id]['secret_number']:
             await message.answer('Мое число меньше')
             users[message.from_user.id]['ATTEMPS'] += 1
@@ -153,15 +129,14 @@ async def process_numbers(message: Message):
             users[message.from_user.id]['ATTEMPS'] = 0
             users[message.from_user.id]['in_game'] = False
             users[message.from_user.id]['total_games'] += 1
-            with open('data.pickle', 'wb') as file:
-                pickle.dump(users, file)
+            save()
     else:
-        keyboard = ReplyKeyboardMarkup(keyboard=key0, resize_keyboard=True, one_time_keyboard=True)
+        keyboard = ReplyKeyboardMarkup(keyboard=button.key0, resize_keyboard=True, one_time_keyboard=True)
         await message.answer('Мы еще не играем. Хотите сыграть?', reply_markup=keyboard)
 
 @dp.message()
 async def other_text_proess(message: Message):
-    keyboard = ReplyKeyboardMarkup(keyboard=key0, resize_keyboard=True, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(keyboard=button.key0, resize_keyboard=True, one_time_keyboard=True)
     if users[message.from_user.id]['in_game']:
         await message.answer('Мы же сейчас с вами играем. '
                              'Присылайте, пожалуйста, числа от 1 до 100')
