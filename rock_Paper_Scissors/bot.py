153b34910747b6f0d594f8d666a3e813f2cd8bfe
Nikitooos123 <120833063+Nikitooos123@users.noreply.github.com>
2023-09-03 22:12:48 +0300

    Добавленно логирование

--- rock_Paper_Scissors/bot.py
+++ rock_Paper_Scissors/bot.py
@@ -1,16 +1,32 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import handler
 
+# Иницилизируем логгер
+logger = logging.getLogger(__name__)
+
+# Функция конфигурирования и запуска бота
 async def main() -> None:
+
+    # Конфигурируем логгирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s'
+    )
+
+    # Выводим в консоль информацию о старте бота
+    logger.info('Starting bot')
 
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
     # Иницилизируем бот и диспетчер
-    bot: Bot = Bot(token=config.tgbot.token)
+    bot: Bot = Bot(token=config.tgbot.token,
+                   parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
 
     # Иницилизируем роутеры в диспетчере
