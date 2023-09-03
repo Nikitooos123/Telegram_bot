45c094bc6a7ab79b03d26f9b5952598a632ca969
Nikitooos123 <120833063+Nikitooos123@users.noreply.github.com>
2023-09-03 22:11:17 +0300

    Хенлеры

--- rock_Paper_Scissors/handlers/handler.py
+++ rock_Paper_Scissors/handlers/handler.py
@@ -5,49 +5,39 @@ from aiogram.types import Message, ReplyKeyboardMarkup
 import random
 from keyboards.keyboard import yes_no_kb, game_kb
 from lexicon.lexicon import LEXICON_RU
+from services.services import bot_choise, determination_of_the_winner
 router: Router = Router()
 
-kb = {
-    'Kaмeнь': 'Бумага',
-    'Бумага': 'Ножницы',
-    'Ножницы': 'Камень'
-}
-
-def random_list() -> str:
-    return random.choice(['Kaмeнь', 'Ножницы', 'Бумага'])
-
+# Хендлер срабатывает на комманду /start
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
 
+# Хендлер срабатывает на команду /help
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
 
+# Хендлер срабатывает на согласие играть
 @router.message(Text(text=LEXICON_RU['yes_button']))
 async def process_game_command(message: Message):
     await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
 
+# Хендлер срабатывать на отказ играть
 @router.message(Text(text=LEXICON_RU['no_button']))
 async def process_game_command(message: Message):
     await message.answer(text=LEXICON_RU['no'], reply_markup=yes_no_kb)
 
+# Хендлер срабатыват на любой выбор игровой кнопки
 @router.message(Text(text=[LEXICON_RU['rock'], LEXICON_RU['paper'], LEXICON_RU['scissors']]))
 async def game_process(message: Message):
-    bot_selection = random_list()
+    bot_selection = bot_choise()
+    winner = determination_of_the_winner(message.text, bot_selection)
     await message.answer(text=LEXICON_RU['bot_choice'] + ': ' + bot_selection)
-
-    if kb[message.text] == bot_selection:
-        await message.answer(text=LEXICON_RU['bot_won'], reply_markup=yes_no_kb)
-
-    elif kb[bot_selection] == message.text:
-        await message.answer(text=LEXICON_RU['user_won'], reply_markup=yes_no_kb)
-
-    elif message.text == bot_selection:
-        await message.answer(text=LEXICON_RU['nobody_won'], reply_markup=yes_no_kb)
+    await  message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
 
 
-
+# Хендлер срабатывает на остальные сообщения
 @router.message()
 async def other_text_process(message: Message):
     await message.answer(text=LEXICON_RU['other_answer'])
