ea811950982fae05b6bda2bf891b1a3ce957b888
Nikitooos123 <120833063+Nikitooos123@users.noreply.github.com>
2023-09-03 22:12:31 +0300

    Логика бота

--- rock_Paper_Scissors/services/services.py
+++ rock_Paper_Scissors/services/services.py
@@ -1,4 +1,29 @@
 import random
 
-def random_list() -> str:
-    return random.choice(['Камень', 'Ножницы', 'Бумага'])
+from lexicon.lexicon import LEXICON_RU
+
+# Функция случайного выбора бота в игре
+def bot_choise() -> str:
+    return random.choice(['rock', 'paper', 'scissors'])
+
+# Возвращает ключ нашего ответа
+def get_key(user_choise: str) -> str:
+    for key in LEXICON_RU:
+        if LEXICON_RU[key] == user_choise:
+            break
+    return key
+
+# Определение победителя
+def determination_of_the_winner(user_choise: str, bot_choise: str) -> str:
+    rules = {
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rock'
+    }
+    user = get_key(user_choise)
+    if rules[user] == bot_choise:
+        return 'user_won'
+    elif user == bot_choise:
+        return 'nobody_won'
+    else:
+        return 'bot_won'
